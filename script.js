467b1b91abe3745439937a273ef3be4b7704d820
Joshua Albrecht <joshua.albrecht@mail.de>
2023-06-08 19:43:50 +0200

    Fixed bug where first/last words were counted

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 "use strict";
 
 function countWords(ignoreCapitalization, maxWordCount) {
-    // Declaring variables
+    // Get text from textarea
     let input = document.getElementById("input").value;
-    const output =  document.getElementById("output");
-    const wordFrequency = {};
 
-    if (ignoreCapitalization) input = input.toLowerCase();  // If true converts string to only lower case
-    
+    // If true converts text to lower case only
+    if (ignoreCapitalization) input = input.toLowerCase();
+
     // Removes characters before the first word and after the last to prevent a bug. Turns string into seperate objects
-    input = input.replace(/^\W+|\W+$/g, '');
+    input = input.replace(/^\W+|\W+$/g, "");
     const words = input.split(/\W+/);
 
+    // Counts and displays the total number of words and returns when there are none
+    const totalWords = words.length - 1;
+    if (!totalWords) return;
+    document.getElementById("totalWords").innerHTML = `Total words: ${totalWords}`;
+
     // Counts frequency of each word
+    const wordFrequency = {};
     for (const i of words)
         i in wordFrequency ? wordFrequency[i]++ : wordFrequency[i] = 1;
 
@@ -20,9 +25,9 @@ function countWords(ignoreCapitalization, maxWordCount) {
     const sortedWords = Object.entries(wordFrequency).sort((a, b) => b[1] - a[1]);
     const frequentWords = sortedWords.slice(0, maxWordCount);
 
-    // Clears old output and displays total amount of words
+    // Clears old output
+    const output =  document.getElementById("output");
     output.innerHTML = "";
-    document.getElementById("totalWords").innerHTML = `Total words: ${words.length - 1}`;
 
     // Adds the elements to the output
     for (const [word, frequency] of frequentWords)
